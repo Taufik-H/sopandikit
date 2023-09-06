b52f9c2ad6d9b1bd89144a46ec94b28181e9f64c
Taufik Hidayatulloh <44672713+Taufik-H@users.noreply.github.com>
2023-09-07 02:41:05 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="w-full flex justify-between items-center border-b border-slaye-400 py-4 px-3">
       <h1 className="font-bold italic">SopanDikit🙏🏻</h1>
       <a
-        href="#"
+        href="https://github.com/Taufik-H/sopandikit/tree/main"
         className="border rounded-md p-2 flex gap-2 items-center hover:bg-slate-100 transition-all duration-200 "
       >
         <HiStar size={20} className="text-yellow-400" />
