1276517d5ed839bc8b8a245ded6f86b9f61a524d
Taufik-H <taufikrpl1@gmail.com>
2023-09-07 10:04:56 +0700

    add:more margin bottom

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Content from "./components/Content";
 function App() {
   return (
-    <div className="App  h-screen w-full bg-white">
+    <div className="App  mb-20 w-full bg-white">
       <Navbar />
       <Content />
     </div>
